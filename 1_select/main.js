0f185d86e4055abf7135062d8993615027bac16d
Estera Kot <esterakot@gmail.com>
2016-06-11 11:05:08 +0200

    Drawing green circle

--- 1_select/main.js
+++ 1_select/main.js
@@ -1,45 +1,48 @@
 // 1_select JavaScript file
 
-
-// selecting <svg> within the document
+// create variable svg
+// selecting <svg> within the document by "svg" tag
 var svg = d3.select("svg");
 
 
-// setting width and height attributes
+// setting width and height attributes to svg variable
 svg.attr("width", 400);
 svg.attr("height", 600);
 
 
 // select <circle> within selected <svg>
-var circle = svg.select("circle");
+//var circle = svg.select("circle");
 
 
 // setting parameters
-circle.attr("cx", 200);           // circle center - x
-circle.attr("cy", 100);           // circle center - y
-circle.attr("r", 50);             // circle radius
-circle.style("fill", "#d62728");  // circle fill color (style, not attr)
+//circle.attr("cx", 200);           // circle center - x
+//circle.attr("cy", 100);           // circle center - y
+//circle.attr("r", 50);             // circle radius
+//circle.style("fill", "#d62728");  // circle fill color (style, not attr)
 
 
 // // but we can make is shorter:
-// // (uncomment below to run)
-// svg.select("circle")
-//   .attr("cx", 200)
-//   .attr("cy", 100)
-//   .attr("r", 50)
-//   .style("fill", "#d62728");
+// (uncomment below to run)
+svg.select("circle")
+   .attr("cx", 200)
+   .attr("cy", 100)
+   .attr("r", 50)
+   .style("fill", "#d62728")
+   .style("opacity", 0.5);
 
 
 // // also, usually we want to create elements with d3.js:
 // // (uncomment below to run)
-// var rect = svg.append("rect")
-//   .attr("x", 50)       // upper left corner - x
-//   .attr("y", 100)      // upper left corner - y
-//   .attr("width", 250)
-//   .attr("height", 20)
-//   .style("fill", "none")
-//   .style("stroke", "#1f77b4")
-//   .style("stroke-width", "4px");
+var rect = svg.append("rect")
+   .attr("x", 50)       // upper left corner - x
+   .attr("y", 100)      // upper left corner - y
+   .attr("width", 250)
+   .attr("height", 20)
+   .style("fill", "none")
+   .style("stroke", "#1f77b4")
+   .style("stroke-width", "4px");
+
+// "rect" -
 
 // NOTE: the last object is being drawn on the top
 
@@ -47,4 +50,9 @@ circle.style("fill", "#d62728");  // circle fill color (style, not attr)
 // 1. Make rect semitransparent (style "opacity" with value 0.5).
 // 2. Draw a new green circle with r=25, cx=50, cy=100.
 
- 
+svg.select("circle")
+    .attr("cx", 50)
+    .attr("cy", 100)
+    .attr("r", 25)
+    .style("fill", "green")
+    .style("opacity", 0.5);
---
+++ README.md
@@ -0,0 +1,7 @@
+Notes from workshops conducted by Piotr Migdał, deepsense.io
+
+D3.js
+
+
+See more at:
+http://bl.ocks.org/mbostock
