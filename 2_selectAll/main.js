6b4135d21d38f51286aabdfd9002d1b7d072db46
Estera Kot <esterakot@gmail.com>
2016-06-11 12:12:53 +0200

    readme and 2 select all

--- 2_selectAll/main.js
+++ 2_selectAll/main.js
@@ -7,36 +7,36 @@ var svg = d3.select("#chart svg")
 	.attr("height", 400);
 
 
-// select only  a single element 
-svg.select("circle").attr("fill", "#d62728");
+// select only a single element
+svg.select("circle").attr("fill", "blue");
 
 
 // // selectAll selects all elements
-// svg.selectAll("circle").attr("fill", "#d62728");
+svg.selectAll("circle").attr("fill", "#d62728");
 
 
 // // but usually we want to select according to class
-// svg.selectAll(".dot").attr("fill", "black");
-// svg.selectAll(".node").attr("fill", "#e377c2");
+svg.selectAll(".dot").attr("fill", "black");
+svg.selectAll(".node").attr("fill", "#e377c2");
 
 
 // // usually we want to modify elements according to data
 // // we need to pass an array
-// var dataForDots = [5, 10, 20];
+var dataForDots = [5, 10, 20];
 
-// svg.selectAll(".dot")
+//svg.selectAll(".dot")
 //   .data(dataForDots)                                        // here we bind data
-//     .attr("r", function (d) { return d; })                  // d goes: 5, 10, 20 
+//     .attr("r", function (d) { return d; })                  // d goes: 5, 10, 20
 //     .attr("cy", function (d, i) { return 200 + 25 * i; });  // i goes: 0, 1, 2
 
 
 // NOTE: if you use the most recent Firefox of Chrome
 // there is a shorter notation with so-called arrow function
 // (a feature of ES2016, a new JavaScript standard)
-// svg.selectAll(".dot")
-//   .data(dataForDots)
-//     .attr("r", (d) => d)
-//     .attr("cy", (d, i) => 200 + 25 * i);
+svg.selectAll(".dot")
+   .data(dataForDots)
+     .attr("r", (d) => d)
+     .attr("cy", (d, i) => 200 + 25 * i);
 // if you have one of these browsers, feel free to use it during this workshop
 // beware, that for production code you need to use 'old' function syntax 
 
@@ -49,23 +49,29 @@ var dataForNodes = [
 	{"size": 3.0, "opacity": 0.3, "kind": 1}   // this extra one is on purpose!
 ];
 
-// try console.log(dataForNodes[0])
-// try console.log(dataForNodes[0].opacity)
+console.log(dataForNodes[0]);
+console.log( "opacity" + dataForNodes[0].opacity);
 
 // // (uncomment below to run)
-// svg.selectAll(".node")
-//   .data(dataForNodes)
-// 	  .attr("r", function (d) { return 10 * d.size; })
-// 	  .style("opacity", function (d) { return d.opacity; })
-// 	  .style("fill", function (d) {
-// 	  	if (d.kind == 0) {
-// 	  		return "#9467bd";
-// 	  	} else {
-// 	  		return "#bcbd22";
-// 	  	}
-// 	  });
+svg.selectAll(".node")
+   .data(dataForNodes)
+	  .attr("r", function (d) { return 10 * d.size; })
+	  .style("opacity", function (d) { return d.opacity; })
+	  .style("fill", function (d) {
+	  	if (d.kind == 0) {
+	  		return "#9467bd";
+	  	} else {
+	  		return "#bcbd22";
+	  	}
+	  });
 
 
 // Exercises
 // 1. For circles with class "node", add stroke "black" with stroke width being same as "size".
+svg.selectAll(".node")
+    .style("stroke", "red")
+    .style("stroke-width", function (d) { return d.size});
+
 // 2. Select all circles and make their cy evenly spaced (e.g. by 50).
+svg.selectAll("circle")
+    .attr("cy", function (d) { return 10 * d.size;});
--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 Notes from workshops conducted by Piotr Migdał, deepsense.io
 
-D3.js
+D3.js - Web Visualization Framework called D3 (Data-Driven Documents)
+to create visualizations that make complex information easier to understand.
+
+D3 is a project featured by the Stanford Visualization Group developed by Mike Bostock.
+
+D3 provides us with web-based visualization, which is an excellent way to deploy information and help us to see things such as proportions, relationships, correlations, and patterns, and discover things previously unknown. Since web browsers provide us with a very flexible and interactive interface in practically any device such as PC, tablet, and smart phone, D3 is an amazing tool for visualization based on data using HTML, JavaScript, SVG, and CSS.
+
+Scalable Vector Graphics (SVG) is an XML-based vector image format for two-dimensional graphics. SVG can be directly included in your web page. SVG provides basic shape elements such as rectangle, line, circle, and text to build complicated lines and shapes inside a canvas. Much of the success of D3 is because it implements a wrapper for SVG. With D3 we will not have to modify the XML directly, instead D3 provides an API to help us place our elements (rectangle, circle, line, and so on) in the correct location on the canvas.
+
+
+Charts:
+Bar chart - Probably, the most common visualization tool is the bar chart. (the horizontal axis (X) represents the category data and the vertical axis (Y) represents a discrete value.)
+
+
 
 
 See more at:
 http://bl.ocks.org/mbostock
+http://techbus.safaribooksonline.com/book/databases/9781783280995/2dot-working-with-data/id286757515?query=%28%28d3js%29%29#X2ludGVybmFsX0h0bWxWaWV3P3htbGlkPTk3ODE3ODMyODA5OTUlMkZpZDI4Njc1ODMxNiZxdWVyeT0oKGQzanMpKQ==
